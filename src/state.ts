d34a505a7f74b3ca5821e4f178f91a7fa7368c37
juvonKT <tkot0003@student.monash.edu>
2023-08-31 11:15:26 +1000

    change activetetrominos to activetetromino ( make it only one)

--- src/state.ts
+++ src/state.ts
@@ -24,21 +24,19 @@ class Tick implements Action {
        */
       apply(s: State): State {
         if (!s.gameEnd) {
-          const activeTetrominos = s.tetrominos.filter((tetromino) => tetromino.id == s.activeTetrominoId)
+          const activeTetromino = s.tetrominos.find((tetromino) => tetromino.id == s.activeTetrominoId)
           const stackedTetrominos = s.tetrominos.filter((tetromino) => tetromino.id != s.activeTetrominoId)
-          const stackedActiveTetrominos = activeTetrominos.some((tetromino) => ((tetromino.position.y + 2) * Block.HEIGHT) == Viewport.CANVAS_HEIGHT)
-          const stackedOnTetrominos = activeTetrominos.some((active) => {
-                return stackedTetrominos.some((stacked) => {
-                  return active.shape.some((activeShape) => {
+          const stackedActiveTetrominos =  ((activeTetromino!.position.y + 2) * Block.HEIGHT) == Viewport.CANVAS_HEIGHT
+          const stackedOnTetrominos = stackedTetrominos.some((stacked) => {
+                  return activeTetromino!.shape.some((activeShape) => {
                     return stacked.shape.some((stackedShape) => {
                       return (
-                        activeShape.x + active.position.x === stackedShape.x + stacked.position.x && 
-                        activeShape.y + active.position.y + 1 === stackedShape.y + stacked.position.y
+                        activeShape.x + activeTetromino!.position.x === stackedShape.x + stacked.position.x && 
+                        activeShape.y + activeTetromino!.position.y + 1 === stackedShape.y + stacked.position.y
                         )
                     })
                   })
                 });
-              })
           
           const tetrominosExceeded = stackedTetrominos.some((tetromino) =>
             tetromino.shape.some((shapePos) =>
@@ -54,9 +52,7 @@ class Tick implements Action {
             const newTetromino = createTetorimino(s.activeTetrominoId + 1, [{x: 0, y: 0}, {x: 1, y: 0}, {x: 0, y: 1}, {x: 1, y: 1}], "blue", {x: 0, y: 0})
             return {...s, tetrominos: [...s.tetrominos, newTetromino], activeTetrominoId: s.activeTetrominoId + 1}
           } else {
-            activeTetrominos.forEach((tetromino) => {
-              tetromino.position.y = tetromino.position.y + 1
-            })
+              activeTetromino!.position.y = activeTetromino!.position.y + 1
             return s
           }
         }
@@ -89,7 +85,7 @@ class Movement implements Action {
           shapePos.y + tetromino.position.y <= 0
         )
       );
-      
+
       if (tetrominosExceeded) {
         return {...s, gameEnd: true}
       }
